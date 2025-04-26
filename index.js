c785bdd94d6cc6f7edcca3bb01f42642813703d3
ATUL0270 <atulgupta0270@gmail.com>
2025-04-26 10:14:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const fs = require("fs")
+
+const PORT = 4000;
+const hostname = "localhost";
+const home = fs.readFileSync("./index.html", "utf-8")
+
+
+const server = http.createServer((req, res) => {
+
+    if (req.url === "/") {
+        return res.end(home);
+    }
+
+    if (req.url === "/about") {
+        return res.end("<h1>ABOUT Page</h1>");
+    }
+
+    if (req.url === "/contact") {
+        return res.end("<h1>CONTACT Page</h1>");
+    }
+    if (req.url === "/service") {
+        return res.end("<h1>SERVICE Page</h1>");
+    }
+    else {
+        return res.end("<h1>404 page not found</h1>");
+    }
+});
+server.listen(PORT, hostname, () => {
+    console.log(`server is working on http://${hostname}:${PORT}`);
+})
+
